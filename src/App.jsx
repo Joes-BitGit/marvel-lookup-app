ea1e505e1d9114a6b8cb7e046bca6bdc81e0fc25
Joes-BitGit <josephnalmeida@gmail.com>
2021-03-31 15:33:58 -0700

    Clear form, Add API error, char error

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,8 @@ import { publicAPI } from "./API.js";
 import Header from "./components/Header.jsx";
 import Loading from "./components/Loading.jsx";
 import CharacterCard from "./components/CharacterCard.jsx";
-
+import CharacterNameForm from "./components/CharacterNameForm.jsx";
+import CharacterError from "./components/CharacterError.jsx";
 // Custom hook that checks initial mount
 import useIsMount from "./components/useIsMount.jsx";
 
@@ -13,7 +14,10 @@ function App() {
   const [url, setUrl] = useState("");
   const [characterData, setCharacterData] = useState();
   const [isLoading, setIsLoading] = useState(false);
-  // Custom Hook
+  // error checking
+  const [isErrorAPI, setisErrorAPI] = useState(false);
+  const [isErrorCharacter, setIsErrorCharacter] = useState(false);
+  // Custom Hook: checks for the initial state of rendering
   const isFirstRender = useIsMount();
 
   useEffect(() => {
@@ -23,16 +27,26 @@ function App() {
       console.log("Subsequent Render");
       // returns a promise
       function fetchMarvelAPI() {
+        setisErrorAPI(false);
+        setIsErrorCharacter(false);
         setIsLoading(true);
         return fetch(url)
           .then((response) => response.json())
-          .then((data) => {
-            // console.log("API FETCH: ", data.data);
+          .then((character) => {
             // returns an object
-            setCharacterData(data.data.results[0]);
+            // console.log("API CHARACTER INFO", character.data);
+            // display an error after you find out the character data is garbage
+            if (character.data.count) {
+              setCharacterData(character.data.results[0]);
+            } else {
+              console.log("CHARACTER NOT FOUND 🤷‍♂️");
+              setIsErrorCharacter(true);
+            }
+
             setIsLoading(false);
           })
           .catch((err) => {
+            setisErrorAPI(true);
             console.warn("ERR, in API call: ", err);
             return null;
           });
@@ -51,30 +65,35 @@ function App() {
     setUrl(
       `https://gateway.marvel.com/v1/public/characters?name=${query}&apikey=${publicAPI}`
     );
+    setQuery("");
   };
 
   return (
     <div className="App">
       <Header />
+      <CharacterNameForm
+        handleSubmit={handleSubmit}
+        handleChange={handleChange}
+        query={query}
+      />
 
-      <form onSubmit={handleSubmit}>
-        <label htmlFor="">
-          Marvel Character Search
-          <br />
-          <input
-            type="text"
-            value={query}
-            placeholder="Hulk, Spider-Man, Iron Man, etc."
-            onInput={handleChange}
-            required
-          />
-        </label>
-        <input type="submit" value="Submit" />
-      </form>
-      {/* Stretch Goal: No need to rerender this part constantly when typing*/}
-      {isLoading ? <Loading /> : characterData && CharacterCard(characterData)}
+      {isErrorAPI && <div>Something Went Horribly Wrong with the API😭</div>}
+      {/* {isErrorCharacter && <div>Character Not Found 🤷‍♂️ </div>} */}
+      {/* <CharacterError isErrorCharacter={isErrorCharacter} /> */}
+      {isErrorCharacter ? (
+        <CharacterError />
+      ) : isLoading ? (
+        <Loading />
+      ) : (
+        characterData && <CharacterCard characterData={characterData} />
+      )}
+      {/* {isLoading ? (
+        <Loading />
+      ) : (
+        characterData && <CharacterCard characterData={characterData} />
+      )} */}
 
-      {characterData && console.log(characterData)}
+      {console.log("Character Data:", characterData)}
       <footer>
         <a href="http://marvel.com">Data provided by Marvel. © 2021 MARVEL</a>
       </footer>
