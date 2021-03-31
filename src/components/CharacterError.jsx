aab6c60837f1fcaf5ae05522f439719c3e62cb7f
Joes-BitGit <josephnalmeida@gmail.com>
2021-03-31 15:32:20 -0700

    Component character error message

---
+++ src/components/CharacterError.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+const CharacterError = () => {
+  return (
+    <div>
+      <div>Character Was Not Found 🤷‍♂️ </div>
+      <div>Can You Try Again Please 🥺 </div>
+    </div>
+  );
+};
+
+export default CharacterError;
