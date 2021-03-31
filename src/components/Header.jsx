7812aa068414bc10fe764b8a434a6eff400f637b
Joes-BitGit <josephnalmeida@gmail.com>
2021-03-30 22:32:43 -0700

    Welcom Message

---
+++ src/components/Header.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+const Header = () => {
+  return (
+    <div>
+      <h1 className="App-header">
+        Welcome to the Marvel Character Search Engine
+      </h1>
+    </div>
+  );
+};
+
+export default Header;
