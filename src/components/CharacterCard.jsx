cce1a113aace3cd93e6eea4f4b3c44315cbe0f6a
Joes-BitGit <josephnalmeida@gmail.com>
2021-04-07 15:35:06 -0700

    horizontal card

--- src/components/CharacterCard.jsx
+++ src/components/CharacterCard.jsx
@@ -2,18 +2,17 @@ import React from "react";
 
 export const CharacterCard = (props) => {
   return (
-    <div className="Marvel-Info">
-      <h2 className="center-align">{props.characterData.name}</h2>
-      <div className="row">
-        <div className="col s12 m4 offset-m4">
-          <div className="card">
-            <div className="card-image">
-              <img
-                src={`${props.characterData.thumbnail.path}/portrait_uncanny.jpg`}
-                alt={`portrait of ${props.characterData.name}`}
-              />
-              <span className="card-title">{props.characterData.name}</span>
-            </div>
+    <div className="Marvel-Info container">
+      <div className="col s12 l6">
+        <h2 className="header">{props.characterData.name}</h2>
+        <div className="card horizontal">
+          <div className="card-image">
+            <img
+              src={`${props.characterData.thumbnail.path}/portrait_uncanny.jpg`}
+              alt={`portrait of ${props.characterData.name}`}
+            />
+          </div>
+          <div className="card-stacked">
             <div className="card-content">
               <p>{props.characterData.description}</p>
             </div>
