d4197a1af736e7ac0ed7bf5027ad162d005e1751
Joes-BitGit <josephnalmeida@gmail.com>
2021-04-06 19:28:04 -0700

    Made the search field larger and submit btn cooler

--- src/components/CharacterNameForm.jsx
+++ src/components/CharacterNameForm.jsx
@@ -2,20 +2,33 @@ import React from "react";
 
 const CharacterNameForm = (props) => {
   return (
-    <form onSubmit={props.handleSubmit}>
-      <label htmlFor="">
-        Marvel Character Search
-        <br />
-        <input
-          type="text"
-          value={props.query}
-          placeholder="Hulk, Spider-Man, Iron Man, etc."
-          onInput={props.handleChange}
-          required
-        />
-      </label>
-      <input type="submit" value="Submit" />
-    </form>
+    <div className="row">
+      <form onSubmit={props.handleSubmit} className="col s12 ">
+        <div className="input-field col s8">
+          <i className="material-icons prefix">account_circle</i>
+          <input
+            type="text"
+            value={props.query}
+            // placeholder="Hulk, Spider-Man, Iron Man, etc."
+            onInput={props.handleChange}
+            className="validate"
+            id="character_name"
+            required
+          />
+          <label htmlFor="character_name">Marvel Character Name</label>
+        </div>
+        <div className="input-field col s4">
+          <button
+            className="btn wave-effect waves-light"
+            type="submit"
+            name="action"
+          >
+            Submit
+            <i className="material-icons right">send</i>
+          </button>
+        </div>
+      </form>
+    </div>
   );
 };
 
