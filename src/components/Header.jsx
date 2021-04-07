97f067c16eff94222f6aab79f46a9abdff4216b5
Joes-BitGit <josephnalmeida@gmail.com>
2021-04-06 19:28:22 -0700

    Colored the navbar

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -2,10 +2,17 @@ import React from "react";
 
 const Header = () => {
   return (
-    <div>
-      <h1 className="App-header">
-        Welcome to the Marvel Character Search Engine
-      </h1>
+    // <div>
+    //   <h1 className="App-header">
+    //     Welcome to the Marvel Character Search Engine
+    //   </h1>
+    // </div>
+    <div className="navbar-fixed">
+      <nav className="nav-wrapper">
+        <a href="#" className="brand-logo">
+          Marvel Search
+        </a>
+      </nav>
     </div>
   );
 };
