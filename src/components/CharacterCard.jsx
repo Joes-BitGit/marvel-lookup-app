782253a18ecfab702ddd5865f109a06e0f02f44a
Joes-BitGit <josephnalmeida@gmail.com>
2021-04-06 19:26:04 -0700

    center text, created a card for character info

--- src/components/CharacterCard.jsx
+++ src/components/CharacterCard.jsx
@@ -3,12 +3,23 @@ import React from "react";
 export const CharacterCard = (props) => {
   return (
     <div className="Marvel-Info">
-      <h2>{props.characterData.name}</h2>
-      <img
-        src={`${props.characterData.thumbnail.path}/portrait_uncanny.jpg`}
-        alt={`portrait of ${props.characterData.name}`}
-      />
-      <p>{props.characterData.description}</p>
+      <h2 className="center-align">{props.characterData.name}</h2>
+      <div className="row">
+        <div className="col s12 m4 offset-m4">
+          <div className="card">
+            <div className="card-image">
+              <img
+                src={`${props.characterData.thumbnail.path}/portrait_uncanny.jpg`}
+                alt={`portrait of ${props.characterData.name}`}
+              />
+              <span className="card-title">{props.characterData.name}</span>
+            </div>
+            <div className="card-content">
+              <p>{props.characterData.description}</p>
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
   );
 };
