28e4596d8ba68555800a69c5e896ba35bf9744f8
Joes-BitGit <josephnalmeida@gmail.com>
2021-04-10 19:54:40 -0700

    list 20 events of character, click to display img

--- src/components/CharacterCard.jsx
+++ src/components/CharacterCard.jsx
@@ -1,6 +1,20 @@
 import React from "react";
+import { publicAPI } from "../API.js";
+// Custom hook that fetches to the marvel api for a character
+import useEventMarvelApi from "./useEventMarvelApi";
 
 export const CharacterCard = (props) => {
+  const handleEventSearch = (uri) => {
+    // display image of event
+    setFetch(`${uri}?apikey=${publicAPI}`);
+  };
+
+  // Custom Hook: checks the marvel api for character
+  const [
+    { eventData, isLoading, isErrorAPI, isErrorCharacter },
+    setFetch,
+  ] = useEventMarvelApi();
+
   return (
     <div className="Marvel-Info container">
       <div className="col s12 l6">
@@ -14,11 +28,30 @@ export const CharacterCard = (props) => {
           </div>
           <div className="card-stacked">
             <div className="card-content">
-              <p>{props.characterData.description}</p>
+              <p>
+                {props.characterData.description !== ""
+                  ? props.characterData.description
+                  : "CLASSIFIED INFORMATION"}
+              </p>
             </div>
           </div>
         </div>
       </div>
+      {/* Display events url here */}
+      <ol>
+        {props.characterData.events.items.map((eventObj, i) => (
+          // use arrow function to not immediately invoke the handleEventSearch
+          <li key={i} onClick={() => handleEventSearch(eventObj.resourceURI)}>
+            {eventObj.name}
+          </li>
+        ))}
+      </ol>
+      {eventData && (
+        <img
+          src={`${eventData.thumbnail.path}/portrait_uncanny.jpg`}
+          alt={`portrait of ${eventData.title}`}
+        />
+      )}
     </div>
   );
 };
